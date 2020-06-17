c308aaa85e1e4dccec837f3180db111157e6669f
telegab5736 <67079301+telegab5736@users.noreply.github.com>
2020-06-17 19:10:45 -0400

    Add files via upload

---
+++ P2T1_SalesPrediction_TelegaBrian.py
@@ -0,0 +1,12 @@
+#Annual Profit
+#17June2020
+#CTI-110 P2T1 - Sales Prediction
+#Brian Telega
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projectd sale:'))
+# Calculate the profit as 23 percent of total sales
+profit = total_sales * .23
+# Display the profit.
+print('The profit is $',format(profit,',.2f'))
+
