c451a37abc7d147bb59cd19e899486c05d20c113
telegab5736 <67079301+telegab5736@users.noreply.github.com>
2020-06-18 04:58:10 -0400

    Add files via upload
    MOD 5 debugging

---
+++ P3LAB_Debugging_BrianTelega.py
@@ -0,0 +1,26 @@
+#CTI-110
+#P3LAB-Debugging
+#Brian Telega
+#18June2020
+
+#pseudocode
+#if the score is greater than or equal to 90 then its an A.
+#else, if the score is greater than or equal to 80 then its a B
+#else, if the score is greater than or equal to 70 then its a C
+#else, if the score is greater than or equal to 60 the its a D
+#else, the grade is a F
+
+
+if score >= A_SCORE:
+    print('Your grade is A.')
+else:
+    if score >= B_SCORE:
+        print('Your grade is B')
+    else:
+        if score >= C_SCORE:
+            print('Your grade is C')
+        else:
+            if score >= D_SCORE:
+                print('Your grade is D')
+            else:
+                print('Your grade is F')
