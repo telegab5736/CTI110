5d484a0c68c03c9add41f5cdbf1fd4313dd23914
telegab5736 <67079301+telegab5736@users.noreply.github.com>
2020-07-06 14:38:06 -0400

    Add files via upload
    P4HW3 Nested loop pattern design with #

---
+++ P4HW3_Nested_loops_BrianTelega.py
@@ -0,0 +1,21 @@
+#Program using nested loops to draw patterns
+#6JULY2020
+#CTI-110P4HW3- Nested Loops
+#Brian Telega
+
+#Create Row, Columns, and Steps
+#Rows needed are 6: 0-5
+
+for row in range(6):
+    print('#',end='',sep='')
+    for space in range(row):
+        print(' ',end='',sep='')
+    print('#',sep='')
+
+
+
+
+
+
+        
+
